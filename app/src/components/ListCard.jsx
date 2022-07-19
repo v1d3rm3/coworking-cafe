62c61f63d0323cc3e43120a062fdc50e09bf5c17
v1d3rm3 <victorschinaider@outlook.com>
2022-07-19 23:23:48 +0000

    wip

--- app/src/components/ListCard.jsx
+++ app/src/components/ListCard.jsx
@@ -1,14 +1,10 @@
-import style from "../styles/ListCard.module.css"
-import data from "../../public/data/data.json"
-import LocalCard from "./LocalCard"
+import style from "../styles/ListCard.module.css";
+import data from "../../public/data/data.json";
+import LocalCard from "./LocalCard";
 
 export default function ListCard() {
-    const renderedList = data.places.map((local) => {
-        return (<LocalCard title={local.name} subtitle={local.address} />)
-    })
-    return (
-        <div className={style.grid}>
-            {renderedList}
-        </div>
-    )
+  const renderedList = data.places.map((local) => {
+    return <LocalCard title={local.name} subtitle={local.address} />;
+  });
+  return <div className={style.grid}>{renderedList}</div>;
 }
--- app/src/components/LocalCard.jsx
+++ app/src/components/LocalCard.jsx
@@ -1,12 +1,15 @@
-import style from "../styles/Card.module.css"
+import style from "../styles/Card.module.css";
 
-export default function LocalCard({title, subtitle}) {
-    return(
-        <a href="/Room">
-            <div className={style.card}>
-                <h3 className={style.title}>{title}</h3>
-                <h6>{subtitle}</h6>
-            </div>
-        </a>
-    );
+export default function LocalCard({ title, subtitle }) {
+  return (
+    <a href="/Room">
+      <div className={style.card}>
+        <div className={style.image}>
+          <img className={style.imageNode} src="https://source.unsplash.com/500x300/?building" />
+        </div>
+        <h3 className={style.title}>{title}</h3>
+        <h6>{subtitle}</h6>
+      </div>
+    </a>
+  );
 }
